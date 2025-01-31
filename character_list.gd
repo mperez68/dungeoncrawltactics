ed6052dc3ca52c991342a73b4b2be2419b0c0289
Marc <marc.perez.dev@gmail.com>
2025-01-30 16:43:49 -0800

    Balance, Polish

--- character_list.gd
+++ character_list.gd
@@ -18,7 +18,7 @@ var music_state = MUSIC_STATE.OFF
 var current_level = LEVEL.LEVEL_1
 
 var generic_options: Array[Node] = [ preload("res://actors/pickups/health_potion_actor.tscn").instantiate(), preload("res://actors/pickups/mana_potion_actor.tscn").instantiate() ]
-var soldier_options: Array[Node] = [ preload("res://actions/block.tscn").instantiate() ]
+var soldier_options: Array[Node] = [ preload("res://actions/block.tscn").instantiate(), preload("res://actions/dash.tscn").instantiate() ]
 var archer_options: Array[Node] = [ preload("res://actions/dash.tscn").instantiate() ]
 var wizard_options: Array[Node] = [ preload("res://actions/immolate.tscn").instantiate() ]
 
